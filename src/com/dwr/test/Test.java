0291e400a83dd597459c7fc1f8d343c7127eb394
na001604 <panzhiyang@cn.fujitsu.com>
2016-04-11 16:15:17 +0800

    飞

--- src/com/dwr/test/Test.java
+++ src/com/dwr/test/Test.java
@@ -10,7 +10,7 @@ public class Test
 <<<<<<< HEAD
 		return "date1234s23";
 =======
-		return "date1234s21";
+		return "datse1234s21";
 >>>>>>> 53f6c16cc0fd8ea69ffc1edc6aa392ccc1018f4e
 >>>>>>> b41cf2055d31bc0879f0c1f0a29b11b4b8846c1a
 	}
