d39b9441df06917a57fb5c916045e2c9ee61521c
na001604 <panzhiyang@cn.fujitsu.com>
2016-04-14 17:37:37 +0800

    士大夫的

--- src/com/dwr/test/Test.java
+++ src/com/dwr/test/Test.java
@@ -4,14 +4,8 @@ public class Test
 {
 	public String getName()
 	{
-<<<<<<< HEAD
+
 		return "date12d34s212";
-=======
-<<<<<<< HEAD
-		return "date1234s23";
-=======
-		return "datse1234s21";
->>>>>>> 53f6c16cc0fd8ea69ffc1edc6aa392ccc1018f4e
->>>>>>> b41cf2055d31bc0879f0c1f0a29b11b4b8846c1a
+
 	}
 }
